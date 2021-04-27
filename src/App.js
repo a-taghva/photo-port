4f1a8d979f8c95edc08fd19c1eebd008064b013f
a-taghva <taghvali99@gmail.com>
2021-04-27 18:07:07 -0400

    ; Gallery component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
-import './App.css';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
