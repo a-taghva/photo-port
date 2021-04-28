bf79f641d73467947d66474c735d28e7c7567c69
a-taghva <taghvali99@gmail.com>
2021-04-28 13:48:20 -0400

    add all photos to ui

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,13 +1,136 @@
-import React from 'react';
-import photo from '../../assets/small/commercial/0.jpg';
+import React, { useState } from 'react';
+
 
 function PhotoList() {
+  const [ photos ] = useState([
+    {
+      name: 'Grocery aisle',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 0
+    },
+    {
+      name: 'Grocery booth',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 1
+    },
+    {
+      name: 'Building exterior',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 2
+    },
+    {
+      name: 'Restaurant table',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 3
+    },
+    {
+      name: 'Cafe interior',
+      category: 'commercial',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 4
+    },
+    {
+      name: 'Cat green eyes',
+      category: 'portraits',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 0
+    },
+    {
+      name: 'Green parrot',
+      category: 'portraits',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 1
+    },
+    {
+      name: 'Yellow macaw',
+      category: 'portraits',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 2
+    },
+    {
+      name: 'Pug smile',
+      category: 'portraits',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 3
+    },
+    {
+      name: 'Pancakes',
+      category: 'food',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 0
+    },
+    {
+      name: 'Burrito',
+      category: 'food',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 1
+    },
+    {
+      name: 'Scallop pasta',
+      category: 'food',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 2
+    },
+    {
+      name: 'Burger',
+      category: 'food',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 3
+    },
+    {
+      name: 'Fruit bowl',
+      category: 'food',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 4
+    },
+    {
+      name: 'Green river',
+      category: 'landscape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 0
+    },
+    {
+      name: 'Docks',
+      category: 'landscape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 1
+    },
+    {
+      name: 'Panoramic village by sea',
+      category: 'landscape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 2
+    },
+    {
+      name: 'Domestic landscape',
+      category: 'landscape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 3
+    },
+    {
+      name: 'Park bench',
+      category: 'landscape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      id: 4
+    },
+  ]);
+
   return(
     <div>
-      <img
-        src={photo}
-        alt="Commercial Example"
-      />
+      <div className="flex-row">
+        {photos.map((img) => (
+          <img
+            src={require(`../../assets/small/${img.category}/${img.id}.jpg`)}
+            alt={img.name}
+            className="img-thumbnail mx-1"
+            key={img.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
