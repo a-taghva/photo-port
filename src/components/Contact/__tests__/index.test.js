7d3832de6253f5b03290ccfa2ba45f745851049d
a-taghva <taghvali99@gmail.com>
2021-04-30 22:07:57 -0400

    contact rendering tests

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup);
+
+describe('contact is rendering', () => {
+  it('renders', () => {
+    render(<Contact />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Contact />);
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  it('matches submit button text', () => {
+    const { getByTestId } = render(<Contact />);
+
+    expect(getByTestId('submitBtn')).toHaveTextContent("Submit");
+  })
+});
