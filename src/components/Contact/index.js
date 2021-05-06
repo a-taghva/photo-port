6c856583a61814b053b4941245fd555b9bf11161
a-taghva <taghvali99@gmail.com>
2021-05-06 00:17:47 -0400

    fix onBlur bug

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -31,15 +31,15 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" value={name} onChange={handleChange} />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" value={email} onChange={handleChange} />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5"  value={message} onChange={handleChange} />
+          <textarea name="message" rows="5"  defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
           <div>
